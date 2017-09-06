41f0a576771455c609a7f5231c35dc63659eb126
huangjunjie <946008612@qq.com>
2017-09-06 13:40:53 +0800

    extension to content script

--- script/main.js
+++ script/main.js
@@ -1,6 +1,30 @@
-console.log('start to inside data');
+console.log('start to content script');
 
-var subject = $('#subject').val();
-console.log(subject)
-$('#subject').val(subject.replace('#2017', ''));
-$('#subject').trigger('change');
+// var subject = $('#subject').val();
+// console.log(subject)
+// $('#subject').val(subject.replace('#2017', ''));
+// $('#subject').trigger('change');
+
+function FillData() {
+  this.init = function() {
+
+  }
+  this.getZWDData = function() {
+    
+  }
+}
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    // console.log(request, sender, sendResponse)
+    console.log(sender.tab ?
+                "from a content script:" + sender.tab.url :
+                "from the extension, fill: " + request.fill);
+    if (request.fill === true) {
+
+      sendResponse({status: 1, msg: "fill success"});
+    }else {
+      sendResponse({status: 0, msg: "not fill"}); // snub them.
+    }
+  }
+);
--- script/popup.js
+++ script/popup.js
@@ -1,8 +1,18 @@
 // ========================
 /* 调试时使用 */
-chrome.tabs.executeScript(null, {file: "script/jquery-3.2.1.slim.min.js"});
+// chrome.tabs.executeScript(null, {file: "script/jquery-3.2.1.slim.min.js"});
 chrome.tabs.executeScript(null, {file: "script/main.js"});
 // ========================
 
-console.log('popup');
+console.log('popup start');
 
+function sendOnekeyFill () {
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    console.log('tabs: ', tabs)
+    chrome.tabs.sendMessage(tabs[0].id, {fill: true}, function(response) {
+      console.log(response);
+    });
+  });
+}
+
+document.getElementById('onekey').addEventListener('click', sendOnekeyFill);
